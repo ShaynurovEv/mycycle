db8b6f9149bd4c12d9d171926998372fe91bb810
Zhenya <shaynurov75@gmail.com>
2023-05-22 20:38:15 +0900

    Initial commit

---
+++ src/test/java/ru/netology/javaqa/mycycle/services/RestServiceTest.java
@@ -0,0 +1,23 @@
+package ru.netology.javaqa.mycycle.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+public class RestServiceTest {
+    @Test
+    public void testOne() {
+        RestService service = new RestService();
+        int expected = 3;
+        int actual = service.calculate(10_000, 3_000, 20_000);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testTwo() {
+        RestService service = new RestService();
+        int expected = 2;
+        int actual = service.calculate(100_000, 60_000, 150_000);
+        Assertions.assertEquals(expected, actual);
+    }
+}
