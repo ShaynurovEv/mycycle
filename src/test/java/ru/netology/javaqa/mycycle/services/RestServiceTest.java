77b88af350fe1dc7ae4d12773efb46c1dc3ebe71
Zhenya <shaynurov75@gmail.com>
2023-05-23 01:19:44 +0900

    Two commit

--- src/test/java/ru/netology/javaqa/mycycle/services/RestServiceTest.java
+++ src/test/java/ru/netology/javaqa/mycycle/services/RestServiceTest.java
@@ -2,22 +2,25 @@ package ru.netology.javaqa.mycycle.services;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 
 public class RestServiceTest {
-    @Test
-    public void testOne() {
+    @ParameterizedTest
+    @CsvFileSource(files="src/test/resources/rest.csv")
+    public void testOne(int expected, int income, int expenses, int threshold) {
         RestService service = new RestService();
-        int expected = 3;
-        int actual = service.calculate(10_000, 3_000, 20_000);
+        //int expected = 3;
+        int actual = service.calculate(income, expenses, threshold);
         Assertions.assertEquals(expected, actual);
     }
 
-    @Test
-    public void testTwo() {
-        RestService service = new RestService();
-        int expected = 2;
-        int actual = service.calculate(100_000, 60_000, 150_000);
-        Assertions.assertEquals(expected, actual);
-    }
+  //  @Test
+   // public void testTwo() {
+       // RestService service = new RestService();
+      //  int expected = 2;
+     //   int actual = service.calculate(100_000, 60_000, 150_000);
+     //   Assertions.assertEquals(expected, actual);
+    //}
 }
